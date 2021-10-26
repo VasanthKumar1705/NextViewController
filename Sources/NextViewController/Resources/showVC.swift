793ad5ef051c682c495b3712a3a6c563892159bb
vasanth <vasanth.kamal@anywhere.co>
2021-10-26 13:58:26 +0530

    intial commit

--- Sources/NextViewController/Resources/showVC.swift
+++ Sources/NextViewController/Resources/showVC.swift
@@ -16,6 +16,13 @@ class showVC : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .yellow
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.text = "I'm a test label"
+
+        self.view.addSubview(label)
     }
     
     
