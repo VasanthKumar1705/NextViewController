917580096b0f5e416a9cc0ed7b6a3f0ddb504b65
vasanth <vasanth.kamal@anywhere.co>
2021-10-26 15:17:37 +0530

    add nextViewcontroller

--- Sources/NextViewController/NextViewController.swift
+++ Sources/NextViewController/NextViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 public struct NextViewController {
+
     public init() {
     }
     
     public func call(ViewController:UIViewController) {
-        let VC = showVC(nibName: "showVC", bundle: nil)
+        let VC = showVC()
         ViewController.present(VC, animated: true, completion: nil)
     }
 }
--- Sources/NextViewController/Resources/showVC.swift
+++ Sources/NextViewController/Resources/showVC.swift
@@ -9,24 +9,18 @@ import Foundation
 import UIKit
 
 class showVC : UIViewController {
-    @IBOutlet var myLabel: UILabel!
-    
-    
-    
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .yellow
-        
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
+        label.center = self.view.center
         label.textAlignment = .center
         label.text = "I'm a test label"
-
         self.view.addSubview(label)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        myLabel.text = "Label 00"
-    }
+
     
 }
